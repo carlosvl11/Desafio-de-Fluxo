0ca6be839b20c16514b8e10b7e592b1ce67936a5
carlosvl11 <carlos_vl1@hotmail.com>
2023-05-29 21:42:15 -0300

    desafio projeto finalizado

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String s) {
+    }
+
+    public boolean getMessage(String s) {
+        return false;
+    }
+}
